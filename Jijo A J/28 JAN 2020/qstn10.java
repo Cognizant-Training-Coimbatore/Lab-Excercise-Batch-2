105ff0e2f1bf88059fcb0ec085d9fbb88c5386c1
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2020-01-28 15:17:50 +0530

    Add files via upload

---
+++ Jijo A J/28 JAN 2020/qstn10.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+  public class qstn10 {
+  public static void main(String[] args) {
+    
+     LinkedList<String> l_list = new LinkedList<String>();
+   l_list.add("Red");
+          l_list.add("Green");
+          l_list.add("Black");
+          l_list.add("Pink");
+          l_list.add("orange");
+      
+      
+   System.out.println("Original linked list:" + l_list);  
+ 
+   // Find first element of the List
+    Object first_element = l_list.getFirst();
+    System.out.println("First Element is: "+first_element);
+ 
+    // Find last element of the List
+    Object last_element = l_list.getLast();
+    System.out.println("Last Element is: "+last_element);
+ }
+}
---
+++ Jijo A J/28 JAN 2020/qstn11.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Iterator;
+  public class qstn11 {
+  public static void main(String[] args) {
+    // create an empty linked list
+     LinkedList<String> l_list = new LinkedList<String>();
+   // use add() method to add values in the linked list
+          l_list.add("Red");
+          l_list.add("Green");
+          l_list.add("Black");
+          l_list.add("Pink");
+          l_list.add("orange");
+      
+      // print original list
+   System.out.println("Original linked list:" + l_list);  
+ 
+   // Find first element of the List
+   System.out.println("Enetr the positions to be swapped");
+   Scanner sc=new Scanner(System.in);
+   int x=sc.nextInt();
+   int y=sc.nextInt();
+    String first_element = l_list.get(x);
+    l_list.set(x,l_list.get(y));
+    l_list.set(y,first_element);
+    
+    System.out.println("Original linked list:" + l_list);  
+    
+ }
+}
