04df71fc95c35c81fa94404ed1f62068df69303e
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2020-01-28 15:35:05 +0530

    Add files via upload

---
+++ Jijo A J/28 JAN 2020/qstn19.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Map.Entry;  
+public class qstn19 {  
+     public static void main(String args[]) {
+
+  // Create a tree map
+  TreeMap <String,String> tree_map1 = new TreeMap <String,String> ();
+
+  // Put elements to the map 
+  tree_map1.put("C2", "Red");
+  tree_map1.put("C1", "Green");
+  tree_map1.put("C4", "Black");
+  tree_map1.put("C3", "White");
+
+  System.out.println("Orginal TreeMap content: " + tree_map1);
+  System.out.println("Greatest key: " + tree_map1.firstKey());
+  System.out.println("Least key: " + tree_map1.lastKey());
+ }
+}
