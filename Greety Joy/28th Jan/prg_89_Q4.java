c93ab9a393b00804a32c42f5ae04f5f95d083d6a
GREETY JOY <60378300+greetzjoy@users.noreply.github.com>
2020-01-28 17:54:35 +0530

    Add files via upload

---
+++ Greety Joy/28th Jan/prg_89_Q4.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class prg_89_Q4 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> map1 = new HashMap<Integer,String>();
+		map1.put(1,"Vinod");
+		map1.put(2,"geethu");
+		map1.put(3,"rohit");
+		map1.put(4,"sanjay");
+		String empname = map1.get(4);
+		System.out.println(empname);
+		
+		
+	}
+
+}
