33d72d64270660441a2c4a001915efcb39161d00
greetzjoy <60378300+greetzjoy@users.noreply.github.com>
2020-01-28 15:24:30 +0530

    Add files via upload

---
+++ Greety Joy/Jan 22nd/JavaDemo.java
@@ -0,0 +1,26 @@
+class AddTwoNos
+{
+int c;
+
+void add(int a, int b)
+{
+c=a+b;
+System.out.println("The addition of two no is"+c);
+}
+}
+
+public class JavaDemo
+
+{
+public static void main(String a[])
+
+{
+AddTwoNos a1 = new AddTwoNos();
+a1.add(10,20);
+}
+}
+
+
+
+
+
