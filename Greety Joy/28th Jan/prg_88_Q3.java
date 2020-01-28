49a282c9e0af604763f3f6cffd0f61f02a8c6176
greetzjoy <60378300+greetzjoy@users.noreply.github.com>
2020-01-28 17:46:54 +0530

    Add files via upload

---
+++ Greety Joy/28th Jan/prg_88_Q3.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class prg_88_Q3 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         ArrayList<Integer> list1 = new ArrayList<Integer>();
+         list1.add(100);
+         list1.add(200);
+         list1.add(300);
+         list1.add(0,400);
+         System.out.println(list1);
+	}
+
+}
