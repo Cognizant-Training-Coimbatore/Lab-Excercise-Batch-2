93c88e1e4309458b350e18176babd27dda50cf2f
Betty Benny <60378337+BettyBenny-1997@users.noreply.github.com>
2020-01-28 00:20:52 -0800

    Add files via upload

---
+++ Betty Benny/Jan 25/prog72_array_5friendsname.java
@@ -0,0 +1,21 @@
+
+public class prog72_array_5friendsname {
+
+	public static void main(String[] args)
+{
+		String n[]=new String[5];
+		n[0]="Achu";
+		n[1]="Ammu";
+		n[2]="Unni";
+		n[3]="Kuttu";
+		n[4]="Monu";
+		System.out.println(n[0].toUpperCase());
+		System.out.println(n[1].toUpperCase());
+		System.out.println(n[2].toUpperCase());
+		System.out.println(n[3].toUpperCase());
+		System.out.println(n[4].toUpperCase());
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Betty Benny/Jan 25/prog76_array_smallest_number.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class prog76_array_smallest_number 
+{
+
+	public static void main(String[] args)
+	{
+		int num,smlr;
+		int number[]=new int[5];
+		int i;
+		System.out.println("Enter number 0");
+		Scanner s=new Scanner(System.in);
+		number[0]=s.nextInt();
+		smlr=number[0];
+		for(i=1;i<5;i++)
+		{
+		System.out.println("Enter number "+i);
+		num=s.nextInt();
+		number[i]=num;
+		if(num<smlr)
+		{
+		smlr=num;
+		}
+		}
+		for(i=0;i<5;i++)
+		{
+		System.out.println("Number are "+number[i]);
+		}
+		System.out.println("Smallest number is "+smlr);
+
+		
+
+
+	}
+
+}
