7373ef17f3741fbc042805cad59d78a3fda48c63
greetzjoy <60378300+greetzjoy@users.noreply.github.com>
2020-01-28 16:20:25 +0530

    Add files via upload

---
+++ Greety Joy/Jan 22nd/prg40_Q8.java
@@ -0,0 +1,19 @@
+class programming
+{
+ programming()
+ {
+  System.out.println("I Love programming language");
+ }
+    programming(String n)
+ {
+  System.out.println("I Love"+n);
+ }
+}
+public class prg40_Q8 {
+ public static void main(String[] args) {
+ 
+  programming obj=new programming();
+  programming obj1=new programming("java");
+  
+ }
+}
