42d783c90cdea67c8ccc1186820e8a8d33745bc4
ShilpaSreedharK <50046378+ShilpaSreedharK@users.noreply.github.com>
2020-01-28 13:21:07 +0530

    Add files via upload

---
+++ Shilpa Sreedhar K/jan21/fcn.js
@@ -0,0 +1,9 @@
+
+function ip_stmts(num1,num2)
+{
+	var num1=document.getElementById("num1").value;
+	var num2=document.getElementById("num2").value;
+	var c=Number(num1)+Number(num2);
+	document.write(c);
+
+}
