9ef5f25675f84b1ffc08446391a7873e559e964b
nikhilakamal <60378125+nikhilakamal@users.noreply.github.com>
2020-01-28 17:55:15 +0530

    Add files via upload

---
+++ Nikhila Kamal/28 jan 2020/pgm_16.java
@@ -0,0 +1,26 @@
+package pgm_excercises_28_jan;
+
+import java.util.HashMap;
+
+public class pgm_16 {
+
+	public static void main(String[] args) {
+		 HashMap<Integer,String> mymap= new HashMap<Integer,String>();  
+
+		  mymap.put(1, "Red");
+
+		  mymap.put(2, "Green");
+
+		  mymap.put(3, "Black");
+
+		  mymap.put(4, "White");
+
+		
+
+		  System.out.println("Size of the hash map: "+mymap.size());
+
+		 }
+
+	}
+
+
