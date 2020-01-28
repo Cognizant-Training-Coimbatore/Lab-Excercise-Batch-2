09d94e1c574af72a254295551f250dfddc1660fd
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2020-01-28 13:33:14 +0530

    Add files via upload

---
+++ Jijo A J/25 JAN 2020/qstn7.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class qstn7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] num={1,2,3,4,6};
+int[] a;
+a=num;
+for(int i=0;i<5;i++)
+{
+System.out.println(a[i]);
+}	}
+
+}
