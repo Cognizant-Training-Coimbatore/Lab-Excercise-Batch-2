153c2e7587739a127f8e45d2eecf8d315b0ece38
ParvathyPadmanabhan <49410927+ParvathyPadmanabhan@users.noreply.github.com>
2020-01-28 13:29:41 +0530

    Add files via upload

---
+++ Parvathy Padmanabhan/Jan 23/Pack1/question1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class question1 
+{
+
+	protected int a =10;
+	protected int b=10;
+	protected int sum=0;
+	public void add()
+	{
+		sum=a+b;
+		System.out.println(sum);
+	}
+}
