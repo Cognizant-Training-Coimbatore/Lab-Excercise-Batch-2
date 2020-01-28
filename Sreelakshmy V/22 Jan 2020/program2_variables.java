489d7e059476329ba558197024c92502b3514f0f
Sree190 <60378247+Sree190@users.noreply.github.com>
2020-01-28 13:48:59 +0530

    Add files via upload

---
+++ Sreelakshmy V/22 Jan 2020/program2_variables.java
@@ -0,0 +1,9 @@
+
+public class program2_variables {
+
+	public static void main(String[] args) {
+		int x=10;
+		System.out.println("The value of X is "+x);
+	}
+
+}
---
+++ Sreelakshmy V/22 Jan 2020/program5_pattern.java
@@ -0,0 +1,10 @@
+
+public class program5_pattern {
+
+	public static void main(String[] args) {
+		System.out.println("*****************");
+		System.out.println("I am learning Java");
+		System.out.println("*****************");
+	}
+
+}
