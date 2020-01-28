133010d4047b4bc9d18581af5f6f0959ace2f8bc
Parvathy Murali <41266227+ParvathyMurali@users.noreply.github.com>
2020-01-28 12:37:43 +0530

    Add files via upload

---
+++ PARVATHY MURALI/21-01-2020/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+class AddTwoNumbers
+{
+public static void main(String a[])
+{
+System.out.println("welcome to selinium training");
+}
+}
+
+
+ 
---
+++ PARVATHY MURALI/21-01-2020/AddTwoNumbers2.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+class AddTwoNumbers2
+{
+public static void main(String a1[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter one number");
+int a = sc.nextInt();
+System.out.println("Enter another number");
+int b = sc.nextInt();
+System.out.println(+ a +" + "+ b +" =" +c);
+
+
+int c = a + b;
+}
+}
