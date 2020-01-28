f59ab47e499c67ab63e58ce9a0a59da8f28c206f
nikhilakamal <60378125+nikhilakamal@users.noreply.github.com>
2020-01-28 15:24:03 +0530

    Add files via upload

---
+++ Nikhila Kamal/28 jan 2020/pgm_1.java
@@ -0,0 +1,22 @@
+package pgm_excercises_28_jan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pgm_1 {
+
+	public static void main(String[] args) {
+		List<String> color=new ArrayList<String>();
+		color.add("blue");
+		color.add("red");
+		color.add("orange");
+		color.add("pink");
+		for(String s:color)
+		{
+			System.out.println(s);
+		}
+		
+
+	}
+
+}
