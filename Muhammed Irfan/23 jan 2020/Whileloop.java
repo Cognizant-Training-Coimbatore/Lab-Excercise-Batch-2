7c26ed78d89cfa9b84ec9b95db1a0025baa190ce
Muhammed Irfan P A <60378293+Irfanmuhammed@users.noreply.github.com>
2020-01-28 13:45:34 +0530

    Add files via upload

---
+++ Muhammed Irfan/23 jan 2020/Whileloop.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i=1;
+      while(i<5)
+      {
+    	  System.out.print("\n"+i);
+    	  i++;
+      }
+    	  
+	}
+
+}
