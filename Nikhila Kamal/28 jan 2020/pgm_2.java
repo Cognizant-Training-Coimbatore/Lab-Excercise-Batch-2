a7c9120918db7c2206172dd53cc1f42deb7a84ec
nikhilakamal <60378125+nikhilakamal@users.noreply.github.com>
2020-01-28 15:27:52 +0530

    Add files via upload

---
+++ Nikhila Kamal/28 jan 2020/pgm_2.java
@@ -0,0 +1,30 @@
+package pgm_excercises_28_jan;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class pgm_2 {
+
+	public static void main(String[] args) 
+	{
+	
+		ArrayList myset=new ArrayList();
+		myset.add("java");
+		myset.add("selenium");
+		myset.add("c++");
+		myset.add("per1");
+		myset.add("unix");
+		myset.add("ada");
+		myset.add("java");
+		System.out.println(myset);
+		Iterator iterator1=myset.iterator();
+		while(iterator1.hasNext())
+		{
+			Object obj=iterator1.next();
+			System.out.println(obj);
+		}
+		
+
+	}
+
+}
