26aca843d8809a9789ac09c3b3200365915ca056
catchmeparvati <60378186+catchmeparvati@users.noreply.github.com>
2020-01-28 00:21:30 -0800

    Add files via upload

---
+++ Parvati P P/21,Jan,2020/function.js
@@ -0,0 +1,8 @@
+
+function ip_stmts(num1,num2)
+{
+	var num1=document.getElementbyId("num1").value;
+    var num2=document.getElementbyId("num2").value;
+	var c=Number(num1)+Number(num2);
+	document.write(c);
+}
