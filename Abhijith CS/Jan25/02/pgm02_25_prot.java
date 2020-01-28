2213c624e86645ba046dd6c0a80ca674238b4dd4
abhics-github <60378322+abhics-github@users.noreply.github.com>
2020-01-28 13:24:23 +0530

    Add files via upload

---
+++ Abhijith CS/Jan25/02/pgm02_25_prot.java
@@ -0,0 +1,8 @@
+package packA;
+public class pgm02_25_prot {
+	int a=10;
+	protected pgm02_25_prot(int b)
+	{
+		System.out.println(b);
+	}
+}
---
+++ Abhijith CS/Jan25/02/pgm2_25_prot1.java
@@ -0,0 +1,19 @@
+package packB;
+
+import packA.pgm02_25_prot;
+class pgm4 extends pgm02_25_prot
+{
+	pgm4(int a)
+	{
+		super(a);
+	}
+}
+
+public class pgm2_25_prot1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pgm4 obj=new pgm4(4);
+	}
+
+}
