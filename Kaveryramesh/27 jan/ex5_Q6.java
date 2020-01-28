a88de30e7f1183f88e71f3d5b972c663b077123d
Kaveryramesh <37046454+Kaveryramesh@users.noreply.github.com>
2020-01-28 15:09:13 +0530

    Add files via upload

---
+++ Kaveryramesh/27 jan/ex5_Q6.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class ex5_Q6 {
+
+	    public static void main(String[] args)throws InterruptedException 
+	    { 
+	        Thread.sleep(10000); 
+	        System.out.println("Hello Geeks"); 
+	    } 
+	} 
