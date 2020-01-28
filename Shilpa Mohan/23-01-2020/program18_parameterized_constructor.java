e776b1c4eda116d03f5e51d5d09bf5f2afcbb1f7
Shilpa Mohan <60378135+shilpamohan5@users.noreply.github.com>
2020-01-28 13:43:54 +0530

    Add files via upload

---
+++ Shilpa Mohan/23-01-2020/program18_parameterized_constructor.java
@@ -0,0 +1,21 @@
+class calc
+{
+	int a,b;
+	calc(int x,int y)
+	{
+		a=x;
+		b=y;
+	}
+	void sumofNumbers()
+	{
+		System.out.println(a+b);
+	}
+}
+public class program18_parameterized_constructor {
+
+	public static void main(String[] args) {
+		calc obj = new calc(5,10);
+		obj.sumofNumbers();
+	}
+
+}
