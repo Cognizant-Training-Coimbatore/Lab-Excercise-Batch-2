056718ecf2984d7a892623338afbc3f3d6b32035
ASHA VIJAYAKUMAR <60378416+vijayakumar27asha19@users.noreply.github.com>
2020-01-28 01:58:44 -0800

    Add files via upload

---
+++ Asha Vijayakumar/23-01-2020/prrogram_81_random.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class prrogram_81_random 
+{
+
+	public static void main(String[] args) 
+	{
+		int i;
+		Random a = new Random();
+		for(i=0;i<10;i++)
+		{
+			System.out.println(a.nextInt(100));
+			
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
