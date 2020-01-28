a31d8556a72d3d11d0f89e2eef58e56d8181a62f
Alan M Shaji <41148501+alanmsa7@users.noreply.github.com>
2020-01-28 03:06:51 -0800

    Update qstn_2801_14.java

--- Alan M Shaji/28-Jan/qstn_2801_14.java
+++ Alan M Shaji/28-Jan/qstn_2801_14.java
@@ -6,6 +6,7 @@ public class qstn_2801_14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TreeSet<Integer> tree=new TreeSet<Integer>();
+		TreeSet<Integer> tree1=new TreeSet<Integer>();
 		tree.add(1);
 		tree.add(2);
 		tree.add(3);
@@ -14,13 +15,21 @@ public class qstn_2801_14 {
 		tree.add(23);
 		tree.add(0);
 		tree.add(43);
-
+		tree1 = (TreeSet)tree.headSet(7);
+		Iterator iterator;
+		iterator = tree1.iterator();
+		System.out.println("Tree set data less than 7: ");     
+		while (iterator.hasNext()){
+			System.out.println(iterator.next() + " ");
+		   }
+		
+		/*
 		for(Integer obj:tree)
 		{
 			if(obj<7)
 				System.out.println(obj);
 		}
-
+*/
 	}
 
 }
