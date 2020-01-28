37bc4ab65f9d90829527672ebded7f79329287af
nikhilakamal <60378125+nikhilakamal@users.noreply.github.com>
2020-01-28 15:18:52 +0530

    Add files via upload

---
+++ Nikhila Kamal/27 jan 2020/pgm_throws_ex.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class pgm_throws_ex {
+	static void validate(int num)throws myexceptions
+
+	{
+
+		if(num<0)
+
+		throw new myexceptions("not valid");
+
+		else
+
+			System.out.println("Welcome to vote");
+
+	}
+
+	
+
+
+
+	public static void main(String[] args) {
+		
+		try
+
+		{
+
+			validate(-23);
+
+		}
+
+		catch(Exception m) {
+
+			System.out.println("Exception occured: "+m);
+
+		}
+
+
+
+	}
+
+
+
+	}
+
+
