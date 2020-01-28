f31ab497f32d66ba87401aa8f58d0d65f622c097
ParvathyPadmanabhan <49410927+ParvathyPadmanabhan@users.noreply.github.com>
2020-01-28 13:30:56 +0530

    Add files via upload

---
+++ Parvathy Padmanabhan/Jan 23/Pack2/program1_1.java
@@ -0,0 +1,15 @@
+package pack2;
+import pack1.question1;
+public class program1_1 extends question1
+{
+	int multiply=a*b;
+	question1 obj1 = new question1();
+
+	public void display() 
+	{
+		obj1.add();
+		System.out.println(multiply);
+		
+	}
+	
+}
---
+++ Parvathy Padmanabhan/Jan 23/Pack2/quest1run.java
@@ -0,0 +1,12 @@
+package pack2;
+
+public class quest1run 
+{
+
+	public static void main(String[] args) 
+	{
+		program1_1 obj = new program1_1();
+		obj.display();
+	}
+
+}
