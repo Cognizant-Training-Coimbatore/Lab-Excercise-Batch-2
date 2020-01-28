fc4492f429acfa0b787275dc5ecd9b0bea9c2b6a
Sree190 <60378247+Sree190@users.noreply.github.com>
2020-01-28 16:46:27 +0530

    Add files via upload

---
+++ Sreelakshmy V/22 Jan 2020/class_.java
@@ -0,0 +1,23 @@
+class test
+{
+	int x=10;
+	void assign(int p)
+			{
+		x=p;
+			}
+	void display()
+	{
+		System.out.println(x);
+
+	}
+}
+public class class_ {
+
+	public static void main(String[] args) {
+		
+test obj=new test();
+obj.assign(10);
+obj.display();
+	}
+
+}
