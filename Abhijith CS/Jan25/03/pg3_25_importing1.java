59e9675e0b64033837a6fa1d2b244fe0e4ee32a5
abhics-github <60378322+abhics-github@users.noreply.github.com>
2020-01-28 13:25:14 +0530

    Add files via upload

---
+++ Abhijith CS/Jan25/03/pg3_25_importing1.java
@@ -0,0 +1,11 @@
+package packB;
+import packA.pgm3_25_importing;
+public class pg3_25_importing1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pgm3_25_importing obj=new pgm3_25_importing();
+		obj.display();
+	}
+
+}
---
+++ Abhijith CS/Jan25/03/pgm3_25_importing.java
@@ -0,0 +1,8 @@
+package packA;
+public class pgm3_25_importing
+{
+	public void display()
+	{
+		System.out.println("hello ......");
+	}
+}
