eb451e3a902e0bf7b27418d91df2c5f7c25ab6de
ardrasurendran <60378314+ardrasurendran@users.noreply.github.com>
2020-01-28 13:21:58 +0530

    Add files via upload

---
+++ Ardra P V/Jan21/function.js
@@ -0,0 +1,7 @@
+function ip_stat(num1,num2) {
+	// body...
+	var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+	var c=Number(num1)+Number(num2);
+	document.write(c);
+}
