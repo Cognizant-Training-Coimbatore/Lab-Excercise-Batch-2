9c2c79eb4467bd7199883ddfd31a31b69e17b5bf
Ponnu Elsa Paul <60378258+minionpep@users.noreply.github.com>
2020-01-29 16:57:09 +0530

    Add files via upload

---
+++ Ponnu Elsa/28-01-2020/program97_question13.java
@@ -0,0 +1,27 @@
+package Tuesday;
+
+import java.util.TreeSet;
+
+public class Question13 {
+
+	public static void main(String[] args) {
+		TreeSet<String> myset=new TreeSet<String>();
+		TreeSet<String> myset1=new TreeSet<String>();
+		myset.add("blue");
+		myset.add("green");
+		myset.add("yellow");
+		myset.add("pink");
+		myset.add("white");
+		System.out.println("first tree set "+myset);
+		
+		myset1.add("red");
+		myset1.add("cyan");
+		System.out.println("second tree set "+myset1);
+		
+		myset1.addAll(myset);
+		System.out.println("new tree set after adding elements of first and second set "+myset1);
+
+	}
+
+}
+
---
+++ Ponnu Elsa/28-01-2020/program97_question14.java
@@ -0,0 +1,28 @@
+package Tuesday;
+
+import java.util.TreeSet;
+
+public class Question14 {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Integer> myset=new TreeSet<Integer>();
+		myset.add(1);
+		myset.add(7);
+		myset.add(10);
+		myset.add(3);
+		myset.add(4);
+		System.out.println(myset);
+		System.out.println("Elements less than 7 in the tree set are "); 
+		for(int x:myset)
+		{
+			if(x<7)
+			{
+				System.out.println(x);
+			}
+		}
+		
+	}
+
+}
+
