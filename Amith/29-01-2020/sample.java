9a2f35da10e758d3daf0848e4911876d15ecb56c
Amith C <60378115+amith1997@users.noreply.github.com>
2020-01-29 17:44:18 +0530

    Add files via upload

---
+++ Amith/29-01-2020/sample.java
@@ -0,0 +1,20 @@
+package project1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class sample {
+
+	public static void main(String[] args) throws Exception 
+	{
+		XSSFWorkbook obj = new XSSFWorkbook();
+		FileOutputStream file=new FileOutputStream(new File("javaexcel.xlsx"));
+		obj.write(file);
+		file.close();
+		System.out.println("file created");
+	}
+
+}
