0096cf7c2b446e12179736a4616f4950df16dd8e
shaheen-1996 <60378385+shaheen-1996@users.noreply.github.com>
2020-01-29 17:44:10 +0530

    Add files via upload

---
+++ Abdulla Shahin/28-01-2020/ex20.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.Map.Entry;  
+public class ex20 
+{  
+       public static void main(String args[])
+       {
+		
+		  // Create a tree map
+		  TreeMap < Integer, String > tree_map1 = new TreeMap < Integer, String > ();
+		
+		  // Put elements to the map 
+		  tree_map1.put(10, "Red");
+		  tree_map1.put(20, "Green");
+		  tree_map1.put(40, "Black");
+		  tree_map1.put(50, "White");
+		  tree_map1.put(60, "Pink");
+		
+		  System.out.println("Orginal TreeMap content: " + tree_map1);
+		  System.out.println("Checking the entry for 10: ");
+		  System.out.println("Key(s): " + tree_map1.headMap(10, true));
+		  System.out.println("Checking the entry for 20: ");
+		  System.out.println("Key(s): " + tree_map1.headMap(20, true));
+		  System.out.println("Checking the entry for 70: ");
+		  System.out.println("Key(s): " + tree_map1.headMap(70, true));
+ }
+}
