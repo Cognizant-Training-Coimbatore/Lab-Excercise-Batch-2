3d011de98f638eaa9e7f7dbd7af0d37063b54069
Kaveryramesh <37046454+Kaveryramesh@users.noreply.github.com>
2020-01-29 17:03:57 +0530

    Add files via upload

---
+++ Kaveryramesh/28 jan/ex6_10.java
@@ -0,0 +1,25 @@
+package project1;
+
+import java.util.LinkedList;
+
+public class ex6_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+LinkedList<String> ob=new LinkedList<String>();
+		
+		ob.add("red");
+		ob.add("blue");
+		ob.add("white");
+		ob.add("yellow");
+		ob.add("pink");
+		System.out.println("The list is:"+ob);
+		System.out.println("The first occurence is:"+ob.getFirst());
+		
+		System.out.println("The last occurence is:"+ob.getLast());
+	}
+
+	}
+
+
---
+++ Kaveryramesh/28 jan/ex6_19.java
@@ -0,0 +1,26 @@
+package project1;
+
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class ex6_19 {
+
+	public static void main(String[] args) 
+	{
+		NavigableMap<Integer, String> m = new TreeMap<>();
+		m.put(1,"one");
+		m.put(2,"two");
+		m.put(3,"three");
+		m.put(4,"four");
+		m.put(5,"five");
+		m.put(6,"six");
+		m.put(7,"seven");
+		m.put(8,"eight");
+		m.put(9,"nine");
+		System.out.println("first key="+m.firstKey());
+		System.out.println("last key="+m.lastKey());
+		
+
+	}
+
+}
