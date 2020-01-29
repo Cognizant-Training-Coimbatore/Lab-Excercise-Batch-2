51562dd899ff8d769d0c416bc493a1662b75b4a1
Alan M Shaji <41148501+alanmsa7@users.noreply.github.com>
2020-01-29 04:20:40 -0800

    Update qstn8.java

--- Alan M Shaji/29-Jan/qstn8.java
+++ Alan M Shaji/29-Jan/qstn8.java
@@ -1,22 +1,23 @@
 package apache;
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class qstn8 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
 		Statement statement=null;
 		ResultSet resultset=null;
 		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-		String msAccDB="";
-		String dbURL="";
+		String msAccDB="D:\\MSa\\JDBC\\JDCB1.mdb";
+		String dbURL="jdbc::ucanaccess:\\"+msAccDB;
 		
-		Connection conn=DriverManager.getConnection(dbURL);
-		statement=conn.createStatement();
+		Connection connection=DriverManager.getConnection(dbURL);
+		statement=connection.createStatement();
 		resultset=statement.executeQuery("SELECT* FROM EMPLOYEE");
 		while(resultset.next())
 			System.out.println(resultset.getString(1));
