a298b83c211f0aafd4866ddb0d7df7fa5956e5e1
GREETY JOY <60378300+greetzjoy@users.noreply.github.com>
2020-01-29 17:36:28 +0530

    Add files via upload

---
+++ Greety Joy/28th Jan/prg_92_Q7.java
@@ -0,0 +1,23 @@
+package packA;
+import java.util.ArrayList;
+
+public class prg_92_Q7 {
+
+	
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	         ArrayList<String> list1 = new ArrayList<String>();
+	         list1.add("preesha");
+	         list1.add("dona");
+	         list1.add("saransh");
+	         list1.add("hema");
+	         System.out.println(list1);
+	         list1.set(1,"paradona");
+	         System.out.println(list1);
+	         
+		}
+
+	}
+
