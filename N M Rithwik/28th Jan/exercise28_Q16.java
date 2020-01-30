17e13b87698dd095ca6c66c554ba3b4f97401a9c
N M Rithwik Subrahmannian <58677818+N-M-Rithwik@users.noreply.github.com>
2020-01-30 17:14:33 +0530

    Add files via upload

---
+++ N M Rithwik/28th Jan/exercise28_Q16.java
@@ -0,0 +1,22 @@
+package apache;
+
+import java.util.TreeMap;
+
+public class ques16 {
+
+	public static void main(String[] args) 
+	{TreeMap<Integer,String> s= new TreeMap<Integer,String>();
+    
+	 
+	   s.put(1,"yellow");
+	   s.put(2,"Black");
+	   s.put(3,"Brown");
+	   s.put(4,"white");
+	   s.put(5,"blue");
+	   s.put(6,"grey");
+		   
+		   System.out.println("Size of : "+s.size());
+
+	}
+
+}
