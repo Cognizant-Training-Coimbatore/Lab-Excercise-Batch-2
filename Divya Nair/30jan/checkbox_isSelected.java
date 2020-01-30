e3aab71564f8836bf3c687a0bb70ac9cdedad2e7
Divya Nair <divya.nair996@gmail.com>
2020-01-30 16:23:30 +0530

    Add files via upload

---
+++ Divya Nair/30jan/checkbox_isSelected.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class checkbox_isSelected {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		WebElement login=driver.findElement(By.linkText("Log in"));
+		login.click();
+		Thread.sleep(3000);
+		WebElement rememberme=driver.findElement(By.name("RememberMe"));
+		boolean res=rememberme.isSelected();
+		if(res==true)
+		{
+			System.out.println("Alraedy exist....");
+			
+		}
+		else
+		{
+			System.out.println("Not Selected..selecting now....");
+			rememberme.click();
+			Thread.sleep(5000);
+		}
+		driver.quit();
+	}
+
+}
