0e986c8c3fab7862dbf9d352db80b7119faa58f4
Parvathy S <60378260+parvathysethu@users.noreply.github.com>
2020-01-30 16:49:18 +0530

    Add files via upload

---
+++ Parvathy S/29-01-2020/problem4.java
@@ -0,0 +1,30 @@
+package project1;
+interface test
+{
+	void square(int x);
+}
+class arithmetic implements test
+{
+
+	@Override
+	public void square(int x) {
+		// TODO Auto-generated method stub
+		System.out.println("Area...."+x*x);
+	}
+}
+class TotestInt extends arithmetic
+{
+	arithmetic ar = new arithmetic();
+	TotestInt(int y)
+	{
+		ar.square(y);
+	}
+}
+public class pgmex4_interparent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TotestInt tti=new TotestInt(5);
+	}
+
+}
