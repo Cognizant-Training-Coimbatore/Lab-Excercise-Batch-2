a925a6758bf2b85c6b1db82293eb59f6a870c809
Jaseem V <jaseemv96@gmail.com>
2020-01-30 17:00:11 +0530

    Add files via upload

---
+++ Jaseem V/21-01-2020/function.js
@@ -0,0 +1,9 @@
+
+
+function ip_statements(num1,num2)
+{
+	var num1=Number(document.getElementById("num1").value);
+	var num2=Number(document.getElementById("num2").value);
+	var c=Number(num1)+Number(num2);
+	document.write(c);
+}
