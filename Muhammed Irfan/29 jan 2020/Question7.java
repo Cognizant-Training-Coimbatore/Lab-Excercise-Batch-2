31567831d4015d98c67f227017c40a31e6cb59df
Muhammed Irfan P A <60378293+Irfanmuhammed@users.noreply.github.com>
2020-01-30 14:04:16 +0530

    Add files via upload

---
+++ Muhammed Irfan/29 jan 2020/Question7.java
@@ -0,0 +1,27 @@
+package demo_29_01_2020;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Question7 {
+
+	public static void main(String[] args) throws Exception
+		{
+		// TODO Auto-generated method stub
+		String datafile ="D:/mydata.csv";
+		BufferedReader br = null;
+		String line="";
+		String cvsSplitBy=",";
+		br = new BufferedReader(new FileReader(datafile));
+		int count=0;
+		while ((line = br.readLine())!=null)
+		{
+			String [] data = line.split(cvsSplitBy);
+			count++;
+			System.out.println(data[0]);
+	    }
+		System.out.println("Number of lines: "+count);
+	}
+}
+
