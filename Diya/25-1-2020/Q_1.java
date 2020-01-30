3b3f9c1bbd45fcbc6dacadb9a65207787f16f01d
diya-mathew <60378166+diya-mathew@users.noreply.github.com>
2020-01-30 14:29:45 +0530

    Add files via upload

---
+++ Diya/25-1-2020/Q_1.java
@@ -0,0 +1,20 @@
+package packtwo;
+
+import packone.q_1;
+
+public class Q_1 extends q_1 {
+	int y=2;
+	void display() 
+	{
+		System.out.println(x+y);
+	}
+	
+
+	public static void main(String[] args) {
+		Q_1 obj =new Q_1();
+		obj.display();
+		
+
+	}
+
+}
---
+++ Diya/25-1-2020/q_1.java
@@ -0,0 +1,6 @@
+package packone;
+
+public class q_1 {
+	protected int x=5;
+
+}
