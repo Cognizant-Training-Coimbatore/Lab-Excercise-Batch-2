0af5f8e69cbc77cb1a838fc689551a23314daa3e
diya-mathew <60378166+diya-mathew@users.noreply.github.com>
2020-01-30 14:27:41 +0530

    Add files via upload

---
+++ Diya/Program1.java
@@ -0,0 +1,8 @@
+package packA;
+
+public class Program1 {
+	int x=5;
+	protected int y=2;
+	
+
+}
---
+++ Diya/Program_1.java
@@ -0,0 +1,10 @@
+package packB;
+
+public class Program_1 {
+	int c=3;
+	void display()
+	{
+		System.out.println(c);
+	}
+
+}
