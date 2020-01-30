c66c1650f3ca2a6b2c95c686608822a5af0ee97b
ananduvk1997 <60378172+ananduvk1997@users.noreply.github.com>
2020-01-30 17:04:44 +0530

    Add files via upload

---
+++ Anandapadmanabhan V K/29 Jan 2020/que7.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class que7 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String dataFile="E://mydata.csv";
+		BufferedReader br= null;
+		String line="";
+		String csvSplit=",";
+		int count =0;
+		br= new BufferedReader(new FileReader(dataFile));
+		while((line=br.readLine())!=null)
+		{
+			count++;
+		}
+		System.out.println(count);
+		br.close();
+		
+
+	}
+
+}
