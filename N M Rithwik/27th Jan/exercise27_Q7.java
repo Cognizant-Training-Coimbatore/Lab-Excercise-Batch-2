9bbb6c78123fc4f6c7f21f610f1874c1d2e1c256
N M Rithwik Subrahmannian <58677818+N-M-Rithwik@users.noreply.github.com>
2020-01-30 17:02:47 +0530

    Update exercise27_Q7.java

--- N M Rithwik/27th Jan/exercise27_Q7.java
+++ N M Rithwik/27th Jan/exercise27_Q7.java
@@ -3,7 +3,17 @@ public class exercise27_Q7 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		if(n>101 && n<200)
+		{
+			System.out.println("input is valid");
+		}
+		else
+		{
+			throw new ArithmeticException("SORRY INVALID INPUT");
+		}
 	}
 
 }
