db9e93c6f07b8ca740eed1e438e1323effdad5ec
Parvathy Murali <41266227+ParvathyMurali@users.noreply.github.com>
2020-01-31 13:31:32 +0530

    Add files via upload

---
+++ PARVATHY MURALI/31-01-2020/p1_31_pattern_java.java
@@ -0,0 +1,21 @@
+/*1.	Write a Java program to display the following pattern. 
+ Sample Pattern :
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a*/
+
+package packone;
+
+public class p1_31_pattern_java 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("  J    a   v     v  a");
+		System.out.println("  J   a a   v   v  a a");
+		System.out.println("J J  aaaaa   V V  aaaaa");
+		System.out.println(" JJ  a   a    V  a     a");
+	}
+
+}
