281cb64c19254d7d2fcbd21e6b8af972f3e7dce6
ardrasurendran <60378314+ardrasurendran@users.noreply.github.com>
2020-01-31 17:40:59 +0530

    Add files via upload

---
+++ Ardra P V/jan31/Q35.java
@@ -0,0 +1,21 @@
+package jan31;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Q35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> list1=new LinkedList<String>();
+		list1.add("e");
+		list1.add("e");
+		list1.add("e");
+		System.out.println("linked list" +list1);
+		list1.addFirst("G");
+		
+        list1.addLast("k");
+        System.out.println("linked list" +list1);
+	}
+
+}
