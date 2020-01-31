96e5b732e086906881e3d4e3d38f434a0497177b
ShilpaSreedharK <50046378+ShilpaSreedharK@users.noreply.github.com>
2020-01-31 17:38:19 +0530

    Add files via upload

---
+++ Shilpa Sreedhar K/jan31/Question20.java
@@ -0,0 +1,12 @@
+package jan31;
+
+public class Question20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {1,2,3,4,5};
+		System.out.println();
+		
+	}
+
+}
---
+++ Shilpa Sreedhar K/jan31/Question47.java
@@ -0,0 +1,32 @@
+package jan31;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Question47 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet <Integer>ts = new TreeSet<Integer>();
+		   TreeSet<Integer> ts1 = new TreeSet<Integer>();
+		  
+		     
+		   ts.add(1);
+		   ts.add(2);
+		   ts.add(3);
+		   ts.add(4);
+		   ts.add(5);
+		   ts.add(7);
+		   ts.add(8);
+		   ts.add(9);
+		   ts.add(10);
+		   System.out.println(ts);
+		   Scanner sc=new Scanner(System.in);
+		   System.out.println("enter an element from the set: ");
+		   int n=sc.nextInt();
+		   ts1=(TreeSet<Integer>)ts.headSet(n, true);
+		   System.out.println("Greater than" +n + ":" +ts1);
+
+	}
+
+}
