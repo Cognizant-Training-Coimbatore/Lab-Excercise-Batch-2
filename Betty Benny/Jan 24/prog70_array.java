0b76b5ca9fdb3880428b7e1e80aa42456f053549
Betty Benny <60378337+BettyBenny-1997@users.noreply.github.com>
2020-01-30 19:59:59 -0800

    Add files via upload

---
+++ Betty Benny/Jan 24/prog70_array.java
@@ -0,0 +1,19 @@
+
+public class prog70_array 
+{
+
+	public static void main(String[] args)
+	{
+		int n[]=new int[5];
+		n[0]=100;
+		n[1]=200;
+		n[2]=300;
+		n[3]=400;
+		n[4]=500;
+		int sum=n[0]+n[1]+n[2]+n[3]+n[4];
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
