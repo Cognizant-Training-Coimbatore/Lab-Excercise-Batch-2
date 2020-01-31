a87851ae42548e8d4d3ccb3f851cf739bb42582c
Betty Benny <60378337+BettyBenny-1997@users.noreply.github.com>
2020-01-30 20:20:28 -0800

    Add files via upload

---
+++ Betty Benny/Jan 22/program9_string.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class program9_string 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1;
+		System.out.println("Enter the string");
+		Scanner scanner=new Scanner(System.in);
+		s1=scanner.nextLine();
+		System.out.println("String length is " +s1.length());
+		// TODO Auto-generated method stub
+
+	}
+
+}
