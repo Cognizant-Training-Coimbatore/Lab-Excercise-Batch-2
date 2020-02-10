58366e49c19c4cc125523692454a948dffebb9e3
jestincolincorreya <60378352+jestincolincorreya@users.noreply.github.com>
2020-02-10 15:22:50 +0530

    Add files via upload

---
+++ JESTIN COLIN CORREYA/10-02-2020/day2_q2.java
@@ -0,0 +1,26 @@
+
+        import org.openqa.selenium.By;
+		import org.openqa.selenium.WebDriver;
+		import org.openqa.selenium.chrome.ChromeDriver;
+		import org.openqa.selenium.support.ui.Select;
+
+
+	
+		public class day2_q2 {
+
+			public static void main(String[] args) throws InterruptedException 
+			{
+				// TODO Auto-generated method stub
+				System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+				WebDriver driver=new ChromeDriver();
+				driver.get("http://examples.codecharge.com/Store/Default.php");
+				System.out.println("Titel\t:\t"+driver.getTitle());
+				Thread.sleep(2000);
+				Select product= new Select(driver.findElement(By.name("category_id")));
+				product.selectByVisibleText("Databases");
+				driver.findElement(By.xpath("/html/body/table[5]/tbody/tr/td[1]/form/table[2]/tbody/tr[3]/td/input")).click();
+				Thread.sleep(5000);
+				driver.quit();
+	}
+
+}
