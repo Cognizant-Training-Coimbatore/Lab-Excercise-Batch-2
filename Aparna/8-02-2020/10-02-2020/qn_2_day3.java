04b2f36ab97aa1e85a1b3f78b3caa0b067af2eb2
aparnaaugustin <60378430+aparnaaugustin@users.noreply.github.com>
2020-02-10 15:27:06 +0530

    Add files via upload

---
+++ Aparna/8-02-2020/10-02-2020/qn_2_day3.java
@@ -0,0 +1,24 @@
+package selenium_new;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class qn_2_day3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\java\\selenium_new\\src\\selenium_new\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://examples.codecharge.com/RegistrationForm/Registration.php");
+		driver.findElement(By.name("user_login")).sendKeys("Admin");
+		driver.findElement(By.name("user_login")).sendKeys(Keys.CONTROL,"a");
+		driver.findElement(By.name("user_login")).sendKeys(Keys.CONTROL,"c");
+		driver.findElement(By.name("user_password")).sendKeys(Keys.CONTROL,"v");
+		Thread.sleep(2000);
+		driver.quit();
+		
+	}
+
+}
