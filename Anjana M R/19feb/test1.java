d58479d840d7c17527e4b41bcaf323d70d519d4b
Anjana M R <60378159+AnjanaMR28@users.noreply.github.com>
2020-03-02 10:28:42 +0530

    Add files via upload

---
+++ Anjana M R/19feb/test1.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class test1 {
+  @Test
+  public void f() {
+	  System.out.println("test1 is running");
+	 
+  }
+}
---
+++ Anjana M R/19feb/test2.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class test2 {
+  @Test
+  public void f() 
+  {
+	  
+	  System.out.println("Test 2 is running");
+  }
+}
